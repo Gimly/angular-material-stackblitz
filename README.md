26818b7eda6075fabb62ebac7d4244bf6d22a2c7
Gimly <xavier.hahn@gmail.com>
2021-06-02 14:03:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-omaraf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-omaraf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
